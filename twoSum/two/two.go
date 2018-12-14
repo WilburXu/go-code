544358a27f0c91ab2d729dbbae2a7251ef4260df
许松涛 <xusongtao@onething.net>
2018-12-14 10:32:00 +0800

    twoSum-two.

--- twoSum/two/two.go
+++ twoSum/two/two.go
@@ -1,9 +1,25 @@
 package main
 
 func main() {
+	var nums = []int{2, 7, 11, 15}
+	var target int = 9
 
+	twoSum(nums, target)
 }
 
-func twoSum(nums []int, target int) {
-	
+func twoSum(nums []int, target int) []int {
+	var vMap = make(map[int]int)
+	for key, num := range nums {
+		vMap[num] = key
+	}
+
+	for i := 0; i < len(nums); i++ {
+		complement := target - nums[i]
+
+		index, ok := vMap[complement]
+		if ok && index != i {
+			return []int{i, index}
+		}
+	}
+	return nil
 }
