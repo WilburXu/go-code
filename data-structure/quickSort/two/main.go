0ce86f414a0ecb5dddc78107f78f6da6bd2f467f
许松涛 <287887940@qq.com>
2018-12-26 23:24:29 +0800

    时间测试。

--- data-structure/quickSort/two/main.go
+++ data-structure/quickSort/two/main.go
@@ -1,14 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	var list = []int{14, 7, 2, 29, 7, 18, 6, 1, 8, 19, 10}
+	list := getTestData(10000000)
+	var startTime int64 = time.Now().UnixNano()/1e6;
 	quickSort(list, 0, len(list)-1)
-	fmt.Println(list)
+	var endTime int64 = time.Now().UnixNano()/1e6;
+	fmt.Println("---------------------")
+	var divider = endTime - startTime;
+	fmt.Println(startTime,endTime);
+	fmt.Println(divider)
+	//fmt.Println(list)
 }
 
-func quickSort(values []int, left int, right int) {
+
+func getTestData(num int32) []int32 {
+	var newArray []int32;
+	maxRan := num * 10;
+	var i int32;
+	for i=0;i<num;i++{
+		newArray = append(newArray,rand.Int31n(maxRan));
+	}
+
+	return newArray;
+}
+
+func quickSort(values []int32, left int, right int) {
 	if left < right {
 		// base number
 		temp := values[left]
