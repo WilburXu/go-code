4eea0afbb4b068c28309b8655c296ff6803f6306
许松涛 <287887940@qq.com>
2020-12-27 22:34:46 +0800

    kafka.

--- kafka/producer/main.go
+++ kafka/producer/main.go
@@ -10,14 +10,15 @@ import (
 
 type Client struct {
 	sarama.AsyncProducer
+	msgPool chan *sarama.ProducerMessage
 }
 
 func NewClient() (*Client, error) {
 	// create client
-	var (
-		err error
-		c   = new(Client)
-	)
+	var err error
+	c := &Client{
+		msgPool: make(chan *sarama.ProducerMessage, 2000),
+	}
 
 	config := sarama.NewConfig()
 	config.Producer.Return.Errors = true
@@ -35,34 +36,54 @@ func NewClient() (*Client, error) {
 	return c, nil
 }
 
-func (c *Client) Send(topic string, msg []byte) (int64, error) {
-	if topic == "" {
-		return 0, errors.New("kafka producer send msg topic empty")
-	}
+func (c *Client) Run() {
+	for {
+		select {
+		case msg := <-c.msgPool:
+			c.Input() <- msg
+		}
 
-	var offset int64
+		select {
+		case suc := <-c.Successes():
+			log.Printf("message: \"%s\" sent to partition %d at offset %d\n", suc.Value, suc.Partition, suc.Offset)
+		case fail := <-c.Errors():
+			log.Printf("err: %s\n", fail.Err.Error())
+		default:
+			log.Printf("ttt %+v \n", c)
+			log.Println("kafka producer msg case default...")
+		}
+	}
+}
+
+func (c *Client) Send(topic string, msg []byte) error {
+	if topic == "" {
+		return errors.New("kafka producer send msg topic empty")
+	}
 
 	kafkaMsg := &sarama.ProducerMessage{
 		Topic: topic,
 		Value: sarama.ByteEncoder(msg),
+		Partition: 2,
 	}
 
-	c.Input() <- kafkaMsg
-	time.Sleep(time.Second * 1)
-	select {
-	case suc := <-c.Successes():
-		offset = suc.Offset
-		log.Printf("message: \"%s\" sent to partition %d at offset %d\n", suc.Value, suc.Partition, suc.Offset)
-	case fail := <-c.Errors():
-		log.Printf("err: %s\n", fail.Err.Error())
-		return 0, fail.Err
-	default:
-		log.Printf("ttt %+v \n", c)
-		log.Println("kafka producer msg case default...")
-		return 0, nil
-	}
+	c.msgPool <- kafkaMsg
 
-	return offset, nil
+	//c.Input() <- kafkaMsg
+	//time.Sleep(time.Second * 1)
+	//select {
+	//case suc := <-c.Successes():
+	//	offset = suc.Offset
+	//	log.Printf("message: \"%s\" sent to partition %d at offset %d\n", suc.Value, suc.Partition, suc.Offset)
+	//case fail := <-c.Errors():
+	//	log.Printf("err: %s\n", fail.Err.Error())
+	//	return 0, fail.Err
+	//default:
+	//	log.Printf("ttt %+v \n", c)
+	//	log.Println("kafka producer msg case default...")
+	//	return 0, nil
+	//}
+
+	return nil
 }
 
 func main() {
@@ -71,53 +92,20 @@ func main() {
 		log.Println("222")
 	}
 
-	i:=1
-	time.Sleep(time.Second * 2)
+	go kafkaProducer.Run()
+
+	i := 1
 	for {
 		tmp := map[string]interface{}{
-			"aa" : i,
-			"bb" : "cc",
+			"aa":   i,
+			"bb":   "cc",
+			"time": time.Now().Format("2006-01-02 15:04:05"),
 		}
 
 		log.Println(i)
 		bs, _ := json.Marshal(tmp)
-		kafkaProducer.Send("omg-live_down", bs)
-		time.Sleep(1*time.Second)
+		kafkaProducer.Send("web_log", bs)
+		time.Sleep(1 * time.Second)
 		i++
 	}
-
-	//config := sarama.NewConfig()
-	//config.Producer.Return.Errors = true
-	//config.Producer.Return.Successes = true
-	//config.Producer.Partitioner = sarama.NewRandomPartitioner
-	//config.Producer.RequiredAcks = sarama.WaitForAll
-	//config.Version = sarama.V1_0_0_0
-	//
-	//producer, _ := sarama.NewAsyncProducer([]string{"127.0.0.1:9092"}, config)
-	//
-	//log.Println("hongbao kfk broker writer runHongbaoBroker")
-	//defer func() {
-	//	log.Println("hongbao kfk broker writer stop")
-	//	producer.Close()
-	//}()
-	//
-	//for {
-	//	log.Println("WilburXu start send msg")
-	//
-	//	kafkaMsg := &sarama.ProducerMessage{
-	//		Topic: "web_log",
-	//		Value: sarama.ByteEncoder("WilburXu send msg"),
-	//	}
-	//	producer.Input() <- kafkaMsg
-	//	select {
-	//	case suc := <-producer.Successes():
-	//		log.Printf("> message: \"%s\" sent to partition  %d at offset %d\n", suc.Value, suc.Partition, suc.Offset)
-	//	case fail := <-producer.Errors():
-	//		log.Printf("err: %s\n", fail.Err.Error())
-	//	default:
-	//		log.Println("???? why?")
-	//	}
-	//
-	//	time.Sleep(time.Second * 1)
-	//}
 }
