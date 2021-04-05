1976b9f0b339f1848c43a518295371a4b5ab01a9
许松涛 <287887940@qq.com>
2021-04-05 19:42:05 +0800

    test

--- test/main.go
+++ test/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 const soh = string(1)
@@ -16,98 +19,140 @@ type LogSt struct {
 }
 
 func main() {
-	testInputFile, _ := os.Open("./log/test/sort/log_0.txt")
-	//testInputFile, _ := os.Open("./access.log")
+	for i := 0; i <= 9; i++{
+		testName := fmt.Sprintf("./log/test/sort/log_%d.txt", i)
+		releaseName := fmt.Sprintf("./log/release/sort/log_%d.txt", i)
+		respName := fmt.Sprintf("./log/release/resp/log_%d.txt", i)
+		test(testName, releaseName, respName)
+	}
+}
+
+func test(testInputFileName string, releaseInputFileName string, respFileName string) {
+	var (
+		err error
+		respFile *os.File
+	)
+
+	testInputFile, _ := os.Open(testInputFileName)
 	testInputReader := bufio.NewReader(testInputFile)
 
-	//var testLine []byte
-	//var testErr error
-
-	releaseInputFile, _ := os.Open("./log/release/sort/log_0.txt")
+	releaseInputFile, _ := os.Open(releaseInputFileName)
 	releaseInputReader := bufio.NewReader(releaseInputFile)
 
-	//releasePos := 0
-	for i := 0; ; i++ {
-		_, _, err := testInputReader.ReadLine()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		//log.Println(string(testLine))
-
-		//testLineSlice := strings.Split(string(testLine), soh)
-		//requestId := testLineSlice[0]
-		//TestRespBody := testLineSlice[1]
-		//Path := testLineSlice[2]
-		//ReleaseRespBody := lineSlice[2]
-
-		for {
-			releaseLine, _, err := releaseInputReader.ReadLine()
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			if _, err := releaseInputReader.Discard(len(releaseLine) + 1); err != nil {
-				log.Println(err)
-				return
-			}
-
-			log.Println(releaseLine)
-		}
-
-		//break
+	if checkFileIsExist(respFileName) {
+		respFile, err = os.OpenFile(respFileName, os.O_APPEND|os.O_WRONLY, 0666) //打开文件
+	} else {
+		respFile, err = os.Create(respFileName)
+	}
+	if err != nil {
+		panic(err)
 	}
 
-	//_, err = testInputFile.Seek(64, 0)
-	//if err != nil {
-	//	log.Println(err)
-	//	return
-	//}
-	//
-	//line, _, err = testInputReader.ReadLine()
-	//log.Println(string(line))
 
-	//fPos := 0 // or saved position
-	//for i := 1; ; i++ {
-	//	testLine, testErr = testInputReader.ReadBytes('\n')
-	//	log.Printf("[line:%d pos:%d] %q\n", i, fPos, testLine)
-	//
-	//	if testErr != nil {
-	//		break
-	//	}
-	//	fPos += len(testLine)
-	//}
+	var (
+		testReadBool     = true
+		testLine         []byte
+		testLineSlice    []string
+		testRequestId    string
+		testRespBody     string
+		testPath         string
+		testLineCnt      int64
+		releaseReadBool  = true
+		releaseLine      []byte
+		releaseLineSlice []string
+		releaseRequestId string
+		releaseRespBody  string
+		releasePath      string
+		releaseLineCnt   int64
+	)
+	for {
+		if testReadBool {
+			testLineCnt++
+			testLine, _, err := testInputReader.ReadLine()
+			if err == io.EOF { // 读到文件末尾就退出
+				log.Println("is over")
+				break
+			}
 
-	//file, _ := os.Open("./access.log")
-	//defer file.Close()
-	//// 偏离位置，可以是正数也可以是负数
-	//var offset int64 = 5
-	//
-	//// 用来计算offset的初始位置
-	//// 0 = 文件开始位置
-	//// 1 = 当前位置
-	//// 2 = 文件结尾处
-	//var whence int = 0
-	//newPosition, err := file.Seek(offset, whence)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//fmt.Println("Just moved to 5:", newPosition)
-	//// 从当前位置回退两个字节
-	//newPosition, err = file.Seek(-2, 1)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//fmt.Println("Just moved back two:", newPosition)
-	//// 使用下面的技巧得到当前的位置
-	//currentPosition, err := file.Seek(0, 1)
-	//fmt.Println("Current position:", currentPosition)
-	//// 转到文件开始处
-	//newPosition, err = file.Seek(0, 0)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//fmt.Println("Position after seeking 0,0:", newPosition)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			testLineSlice = strings.Split(string(testLine), soh)
+			testRequestId = testLineSlice[0]
+			testRespBody = testLineSlice[1]
+			testPath = testLineSlice[2]
+			testReadBool = false
+		}
+
+		if releaseReadBool {
+			releaseLineCnt++
+			releaseLine, _, err := releaseInputReader.ReadLine()
+			if err == io.EOF { // 读到文件末尾就退出
+				log.Println("is over")
+				break
+			}
+
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			releaseLineSlice = strings.Split(string(releaseLine), soh)
+			releaseRequestId = releaseLineSlice[0]
+			releaseRespBody = releaseLineSlice[1]
+			releasePath = releaseLineSlice[2]
+			releaseReadBool = false
+		}
+
+		log.Printf("test cnt %d, release cnt %d, test reqId %s, release reqId %s \n", testLineCnt, releaseLineCnt, testRequestId, releaseRequestId)
+
+		if testRequestId < releaseRequestId {
+			releaseReadBool = true
+			if _, err = releaseInputFile.Seek(int64(len(releaseLine)), 1); err != nil {
+				log.Println(err)
+				continue
+			}
+		} else if testRequestId > releaseRequestId {
+			testReadBool = true
+			if _, err = testInputFile.Seek(int64(len(testLine)), 1); err != nil {
+				log.Println(err)
+				continue
+			}
+		} else {
+			testReadBool = true
+			releaseReadBool = true
+
+			var respRet bool
+			if testRespBody == releaseRespBody {
+				respRet = true
+			}
+			sortStr := fmt.Sprintf("%s%s%s%s%s%s%s%s%s%s%t\n", testRequestId, soh, testRespBody, soh, releaseRespBody, soh, testPath, soh, releasePath, soh, respRet)
+
+			// 创建新的 Writer 对象
+			w := bufio.NewWriter(respFile)
+			_, err = w.WriteString(sortStr)
+			if err != nil {
+				log.Println(err)
+			}
+			_ = w.Flush()
+
+			if _, err = releaseInputFile.Seek(int64(len(releaseLine)), 1); err != nil {
+				log.Println(err)
+				continue
+			}
+
+			if _, err = testInputFile.Seek(int64(len(testLine)), 1); err != nil {
+				log.Println(err)
+				continue
+			}
+		}
+	}
+}
+
+
+func checkFileIsExist(filename string) bool {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
